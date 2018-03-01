2017384a0e259831f29f6441492e5c8bb2c90891
Michael Levett <ml3v3tt@gmail.com>
2018-02-28 19:42:38 -0800

    Initial Commit

---
+++ Selfiegram App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfiegram App
+//
+//  Created by Michael Levett on 2018-02-28.
+//  Copyright © 2018 Michael Levett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
