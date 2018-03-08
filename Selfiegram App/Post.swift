ac8b70e65989a72163b678580d1bb4f1745f3987
Michael Levett <ml3v3tt@gmail.com>
2018-03-07 20:43:48 -0800

    W2 L2

---
+++ Selfiegram App/Post.swift
@@ -0,0 +1,24 @@
+//
+//  Post.swift
+//  Selfiegram App
+//
+//  Created by Michael Levett on 2018-03-07.
+//  Copyright © 2018 Michael Levett. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Post {
+    
+    let SelfieImage:UIImage
+    let user:User
+    let comment:String
+    
+    init(aSelfieImage: UIImage, aUser: User, aComment: String) {
+        self.Image = image
+        self.User = user
+        self.Comment = comment
+    }
+    
+}
---
+++ Selfiegram App/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  Selfiegram App
+//
+//  Created by Michael Levett on 2018-03-07.
+//  Copyright © 2018 Michael Levett. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class User {
+    
+    let username:String
+    let profileImage:UIImage
+    
+    init (aUsername: String, aProfileImage: UIImage) {
+    
+    username = aUsername
+    profileImage = aProfileImage
+    }
+    
+    
+}
